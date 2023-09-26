1065b0ab13b98b69a0dc5caeb33a741aa6d74b87
Volkov Alexander <volkov.525@gmail.com>
2023-09-26 13:56:36 +0300

    remove pic on home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -8,7 +8,7 @@ q-page(padding)
 	.container
 		h4 Цифровой двойник
 		.text-h6 Система on-line предиктивного анализа работоспособности энергетических объектов с использованием технологии искусственного интеллекта и «цифрового двойника».
-		q-img(src="@/assets/img/bliz.jpg" width="500px")
+		// q-img(src="@/assets/img/bliz.jpg" width="500px")
 		p Сюда бы хорошо графики поместить. Сделать красивый дашборд.
 		// SvgIcon(name="logo")
 		// SvgIcon(name="nodeFolder" color="#ccc")
